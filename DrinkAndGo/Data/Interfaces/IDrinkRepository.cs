118aa097d412c9919cfc140be2019e5b27c56c0c
AIexoaner <AIexoaner>
2017-09-17 19:48:32 +0200

    Adding two Interfaces
    Adding interfaces ICategoryRepository and IDrinkRepository to Data folder

---
+++ DrinkAndGo/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DrinkAndGo.Data.Models;
+
+namespace DrinkAndGo.Data.Interfaces
+{
+    interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PreferredDrinks { get; set; }
+        Drink GetDrinkById(int drinkId);
+    }
+}
