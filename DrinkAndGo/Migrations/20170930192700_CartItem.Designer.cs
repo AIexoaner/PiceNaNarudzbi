fd69f96f95824c427814f000f4e9cfd108738ed6
AIexoaner <AIexoaner>
2017-09-30 21:31:00 +0200

    Adding the shopping cart models
    Adding the shopping cart models

--- DrinkAndGo/Migrations/20170930192700_CartItem.Designer.cs
+++ DrinkAndGo/Migrations/20170930192700_CartItem.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DrinkAndGo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170930192700_CartItem")]
+    partial class CartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
