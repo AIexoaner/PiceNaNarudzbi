0baaf6d99cbe5268b973ef3d898af3675cead324
AIexoaner <AIexoaner>
2017-10-01 16:20:39 +0200

    Adding the home controller, models and views
    Adding the home controller, models and views

---
+++ DrinkAndGo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.ViewModels;
+
+namespace DrinkAndGo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+
+        public HomeController(IDrinkRepository drinkRepository)
+        {
+            _drinkRepository = drinkRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredDrinks = _drinkRepository.PreferredDrinks
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
