ad5c6d4c5acf57996808077f0d7752f5dd47e26b
AIexoaner <AIexoaner>
2017-10-05 21:31:58 +0200

    Adding orders models
    Adding orders models and DbContext for updating database

--- DrinkAndGo/Migrations/20171005192856_Orders.Designer.cs
+++ DrinkAndGo/Migrations/20171005192856_Orders.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DrinkAndGo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171005192856_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
